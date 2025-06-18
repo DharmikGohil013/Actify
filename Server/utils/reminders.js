54771f914c4f64beb796dadbb61942346a340332
DharmikGohil013 <dharmikgohil395003@gmail.com>
2025-06-18 23:10:13 +0530

    add

---
+++ Server/utils/reminders.js
@@ -0,0 +1,18 @@
+// /config/db.js
+const mongoose = require('mongoose');
+
+const connectDB = async () => {
+  try {
+    const conn = await mongoose.connect(process.env.MONGO_URI, {
+      useNewUrlParser: true,
+      useUnifiedTopology: true,
+      // useCreateIndex: true,  // not needed for Mongoose 6+
+    });
+    console.log(`MongoDB Connected: ${conn.connection.host}`);
+  } catch (err) {
+    console.error('MongoDB connection failed:', err.message);
+    process.exit(1);
+  }
+};
+
+module.exports = connectDB;
