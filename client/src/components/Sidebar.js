48b1b8e6f708a633452a91fdc2c7b284323ab5bc
DharmikGohil013 <dharmikgohil395003@gmail.com>
2025-07-13 15:09:02 +0530

    Silder corrwect

--- client/src/components/Sidebar.js
+++ client/src/components/Sidebar.js
@@ -131,7 +131,7 @@ export default function Sidebar() {
           <path d="M19 3H5c-1.1 0-2 .9-2 2v14c0 1.1.9 2 2 2h14c1.1 0 2-.9 2-2V5c0-1.1-.9-2-2-2zm-5 14H7v-2h7v2zm3-4H7v-2h10v2zm0-4H7V7h10v2z"/>
         </svg>
       ),
-      badge: "3" 
+     
     },
     { 
       to: "/calendar", 
@@ -159,7 +159,7 @@ export default function Sidebar() {
           <path d="M12 22c1.1 0 2-.9 2-2h-4c0 1.1.89 2 2 2zm6-6v-5c0-3.07-1.64-5.64-4.5-6.32V4c0-.83-.67-1.5-1.5-1.5s-1.5.67-1.5 1.5v.68C7.63 5.36 6 7.92 6 11v5l-2 2v1h16v-1l-2-2z"/>
         </svg>
       ),
-      badge: "2" 
+      
     },
     { 
       to: "/settings", 
